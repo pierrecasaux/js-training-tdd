4f5c28c4836e608d940d313dc40e164ae8d6ca96
pierrekzo <pierrecasaux@gmail.com>
2019-09-28 21:33:30 +0200

    last f*** quest

--- exercises/get-first.js
+++ exercises/get-first.js
@@ -9,12 +9,16 @@
 
 // Your code:
 
+function getFirst(a) {
+  return a[0];
+}
+
 //* Begin of tests
 const assert = require('assert');
 
-assert.strictEqual(getFirst([ 2, 42 ]), 2);
-assert.strictEqual(getFirst([ 'pouet', 4, true ]), 'pouet');
-assert.strictEqual(getFirst([ getFirst ]), getFirst);
+assert.strictEqual(getFirst([2, 42]), 2);
+assert.strictEqual(getFirst(['pouet', 4, true]), 'pouet');
+assert.strictEqual(getFirst([getFirst]), getFirst);
 assert.strictEqual(getFirst('salut'), 's');
 assert.strictEqual(getFirst([]), undefined);
 // End of tests */
--- exercises/get-length.js
+++ exercises/get-length.js
@@ -9,13 +9,18 @@
 
 // Your code:
 
+const getLength = (a) => {
+  return (a).length
+};
+
 //* Begin of tests
 const assert = require('assert');
 
-assert.strictEqual(getLength([ 2, 42 ]), 2);
-assert.strictEqual(getLength([ 'pouet', 4, true ]), 3);
+assert.strictEqual(getLength([2, 42]), 2);
+assert.strictEqual(getLength(['pouet', 4, true]), 3);
 assert.strictEqual(getLength(Array(100)), 100);
-assert.strictEqual(getLength([ getLength ]), 1);
+assert.strictEqual(getLength([getLength]), 1);
 assert.strictEqual(getLength('salut'), 5);
 assert.strictEqual(getLength([]), 0);
-// End of tests */
+
+// End of tests*/
